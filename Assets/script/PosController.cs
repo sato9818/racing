97c6dfc6c26df0c51cf0cf0cc2073247ad7f47c1
sato9818 <kouki9818@gmail.com>
2019-01-25 18:18:52 +0900

    Revert "update"
    This reverts commit 7e8302e6bab5d2a027c5ccde87c93efe51fbcd29.

---
+++ Assets/script/PosController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PosController : MonoBehaviour
+{
+
+    public Transform Camera;
+    public Transform Unitychan;
+    public Transform Head;
+
+    void Update()
+    {
+        Unitychan.position = Camera.position - (Head.position - Unitychan.position);
+    }
+}
